f741e43d877b3261d28af54ac1ca036ac7dbea8a
Henry M <henrymazaika@gmail.com>
2020-09-27 18:58:22 -0700

    removed examples

--- RobotArm/src/main/java/frc/robot/RobotContainer.java
+++ RobotArm/src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,7 @@ import com.team7419.PaddedXbox;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.MoveServo;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
