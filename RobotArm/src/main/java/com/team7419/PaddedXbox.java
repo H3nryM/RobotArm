c1450cf96423886dd47e7668ef1fabbfd9b23b69
Henry M <henrymazaika@gmail.com>
2020-09-27 17:52:33 -0700

    test 1

---
+++ RobotArm/src/main/java/com/team7419/PaddedXbox.java
@@ -0,0 +1,136 @@
+package com.team7419;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class PaddedXbox extends XboxController{
+
+    public PaddedXbox(){
+		super(F310Map.f310Main.value);
+    }
+
+    public enum F310Map{
+        //Input Map
+        f310Main(0),
+        
+        f310Secondary(1),
+        
+        //F310 MAP
+        kGamepadAxisLeftStickX(0),
+        kGamepadAxisLeftStickY(1),
+        kGamepadAxisLeftTrigger(2),
+        kGamepadAxisRightTrigger(3),
+        kGamepadAxisRightStickX(4),
+        kGamepadAxisRightStickY(5),
+        kGamepadAxisDpad(6),
+        kGamepadButtonA(1),
+        kGamepadButtonB(2),
+        kGamepadButtonX(3),
+        kGamepadButtonY(4),
+        kGamepadButtonShoulderL(5),
+        kGamepadButtonShoulderR(6),
+        kGamepadButtonBack(7),
+        kGamepadButtonStart(8),
+        kGamepadButtonLeftStickButton(9),
+        kGamepadButtonRightStickButton(10),
+        kGamepadButtonMode(-1),
+        kGamepadButtonLogitech(-1);
+        
+        public final int value;
+        F310Map(int value) {
+            this.value = value;
+        }	
+    }
+
+    public double getRightX() {
+		double out = getRawAxis(F310Map.kGamepadAxisRightStickX.value);
+		if(Math.abs(out)<.02) {
+			out = 0;
+		}
+		return out;
+	}
+	public double getLeftX() {
+		double out = getRawAxis(F310Map.kGamepadAxisLeftStickX.value);
+		if(Math.abs(out)<.02) {
+			out = 0;
+		}
+		return out;
+	}
+	public double getLeftY() {
+		double out = -getRawAxis(F310Map.kGamepadAxisLeftStickY.value);
+		if(Math.abs(out)<.02) {
+			out = 0;
+		}
+		return -out;
+	}
+	public double getRightY() {
+		double out = -getRawAxis(F310Map.kGamepadAxisRightStickY.value);
+		if(Math.abs(out)<.02) {
+			out = 0;
+		}
+		return out;
+	}
+	public double getLeftTrig() {
+		double out = getRawAxis(F310Map.kGamepadAxisLeftTrigger.value);
+		if(Math.abs(out)<.05) {
+			out = 0;
+		}
+		return -out;
+	}
+	public double getRightTrig() {
+		double out = getRawAxis(F310Map.kGamepadAxisRightTrigger.value);
+		if(Math.abs(out)<.05) {
+			out = 0;
+		}
+		return out;
+	}
+
+
+	public JoystickButton getA() {
+		JoystickButton button = new JoystickButton(this, F310Map.kGamepadButtonA.value);
+		return button;
+	}
+
+	public JoystickButton getB() {
+        JoystickButton button = new JoystickButton(this, F310Map.kGamepadButtonB.value);
+		return button;
+	}
+	
+	/**
+	 * I know the name is dumb, but the super class XboxController has getY and getYButton
+	 * already taken with different types, "double" and "boolean" so... what other choice did I have!?
+	 * @return Y button value
+	 */
+	public JoystickButton getYButtonValue() {
+		JoystickButton button = new JoystickButton(this, F310Map.kGamepadButtonY.value);
+		return button;
+	}
+	
+	/**
+	 * I know the name is dumb, but the super class XboxController has getX and getXButton
+	 * already taken with different types, "double" and "boolean" so... what other choice did I have!?
+	 * @return X button value
+	 */
+	public JoystickButton getXButtonValue() {
+		JoystickButton button = new JoystickButton(this, F310Map.kGamepadButtonX.value);
+		return button;
+	}
+
+	public JoystickButton getRightShoulder() {
+		JoystickButton button = new JoystickButton(this, F310Map.kGamepadButtonShoulderR.value);
+		return button;
+	}
+
+	public JoystickButton getLeftShoulder() {
+		JoystickButton button = new JoystickButton(this, F310Map.kGamepadButtonShoulderL.value);
+		return button;
+	}
+
+	public int getDpad(){
+		return getPOV();
+	}
+
+}
--- RobotArm/src/main/java/frc/robot/RobotContainer.java
+++ RobotArm/src/main/java/frc/robot/RobotContainer.java
@@ -1,43 +1,28 @@
 package frc.robot;
 
+import com.team7419.PaddedXbox;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.MoveServo;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
 
+  private final PaddedXbox joystick = new PaddedXbox();
   private final Servo testServo = new Servo(1);
 
 
-
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
   private void configureButtonBindings() {
+    joystick.getA()
+    .whenPressed(new MoveServo(testServo, 0.5));
   }
 
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
 }
