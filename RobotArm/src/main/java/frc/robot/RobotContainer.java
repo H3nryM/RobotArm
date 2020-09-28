cbd54a082b3b66078c035ff8a6e8d2c1dd70cf51
Henry M <henrymazaika@gmail.com>
2020-09-27 17:03:47 -0700

    committing random garabage

--- RobotArm/src/main/java/frc/robot/RobotContainer.java
+++ RobotArm/src/main/java/frc/robot/RobotContainer.java
@@ -1,29 +1,15 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Servo testServo = new Servo(1);
 
 
 
---
+++ RobotArm/src/main/java/frc/robot/commands/MoveServo.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class MoveServo extends CommandBase {
+  
+  private Servo servo;
+  private double position;
+
+  /**
+   * Creates a new MoveServo.
+   */
+  public MoveServo(Servo servo, double position) {
+  this.servo = servo;
+  this.position = position;
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+    servo.setAngle(position);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+  
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+}
