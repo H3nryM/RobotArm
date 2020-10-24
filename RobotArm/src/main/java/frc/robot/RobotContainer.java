b2418229f9e71057bc77ca313c231f09dbb98915
Henry M <henrymazaika@gmail.com>
2020-10-24 13:45:07 -0700

    Updated MoveServo to only have the MyServo constructor, not both Servo and MyServo

--- RobotArm/src/main/java/frc/robot/RobotContainer.java
+++ RobotArm/src/main/java/frc/robot/RobotContainer.java
@@ -32,6 +32,16 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
+    // joystick.getA()
+    // .whenPressed(new MoveServo(hingeServo, 75));
+    joystick.getB()
+    .whenPressed(new MoveServo(hingeMyServo, 65));
+    joystick.getA()
+    .whenPressed(new MoveServo(hingeMyServo, 35));
+    joystick.getXButtonValue()
+    .whenPressed(new MoveServo(hingeMyServo, 150));
+    joystick.getYButtonValue()
+    .whenPressed(new MoveServo(hingeMyServo, 90));
 
   }
 
--- RobotArm/src/main/java/frc/robot/servo/MoveServo.java
+++ RobotArm/src/main/java/frc/robot/servo/MoveServo.java
@@ -6,24 +6,14 @@ import frc.robot.servo.MyServo;
 
 public class MoveServo extends CommandBase {
   
-  private Servo servo;
   private MyServo myServo;
   private double position;
   private boolean done;
   private double iT;
   private double runCount;
-  private boolean isServo;
 
   /**
-   * Creates a new Servo MoveServo.
-   */
-  public MoveServo(Servo servo, double position) {
-  this.servo = servo;
-  this.position = position;
-  }
-
-  /**
-   * Creates a new MyServo MoveServo.
+   * Creates a new MoveServo.
    */
   public MoveServo(MyServo myServo, double position) {
     this.myServo = myServo;
@@ -40,29 +30,19 @@ public class MoveServo extends CommandBase {
   public void execute() {
     runCount++;
     if(runCount==1){
-      if(servo!=null){isServo=true;}
-      else{isServo=false;}
       iT=System.currentTimeMillis();
       }
-      if(isServo){
-      System.out.println(servo.getAngle());
-      servo.setAngle(position);
-      if(System.currentTimeMillis() - iT > 500 && servo.getAngle() == position){
-        done = true;
-      }
-    } else {
       System.out.println(myServo.getAngle());
       myServo.setAngle(position);
       if(System.currentTimeMillis() - iT > 500){
         done = true;
-      }
+      
     }
   }
 
   @Override
   public void end(boolean interrupted) {
-    if(isServo){servo.stopMotor();}
-    else{myServo.stopMotor();}
+    myServo.stopMotor();
     done = false;
     runCount = 0;
     System.out.println("done");
