89b9c53578cb393422849ab42c5387a82976a0b5
Henry M <henrymazaika@gmail.com>
2020-10-28 14:48:43 -0700

    made my best guess for the limelight sub values. we'll have to see how it works.

--- RobotArm/src/main/java/frc/robot/limelight/LimelightSub.java
+++ RobotArm/src/main/java/frc/robot/limelight/LimelightSub.java
@@ -11,19 +11,20 @@ public class LimelightSub extends SubsystemBase{
     public LimelightSub(){}
 
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-
+    NetworkTableEntry tx = table.getEntry("tx"); 
+    NetworkTableEntry ty = table.getEntry("ty"); 
+    
     String current;
 
     @Override
     public void periodic(){
+        // to test limelight's functionality
         double x = tx.getDouble(0);
         double y = ty.getDouble(0);
         SmartDashboard.putNumber("tx", x);
         SmartDashboard.putNumber("ty", y);
     }
 
-    public double getTx(){return tx.getDouble(0);}
-    public double getTy(){return ty.getDouble(0);}
+    public double getTx(){return -ty.getDouble(0);} // negated and tx is ty due to cam rotation
+    public double getTy(){return -tx.getDouble(0);} // negated and ty is tx due to cam rotation
 }
